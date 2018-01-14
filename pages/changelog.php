b1953f49dc89c18f6e560f3da59d3cd7b23e33c7
louis.jd.martin <louis.jd.martin@gmail.com>
2018-01-14 21:06:13 +0100

    Mise a jour du changelog

--- pages/changelog.php
+++ pages/changelog.php
@@ -1,3 +1,37 @@
 <!-- Changer /includes/fonctions.php ligne 5 !-->
+<strong>V0.0</strong>
+<br />- Première version pour les campagnes BDE
+
+<br />
+<strong>V0.1</strong>
+<br />- Génération automatique de planning
+
+<br />
+<strong>V0.2 et plus</strong>
+<br />- Amélioration de l'algo de génération en fonction du dernier planning généré
+
+<br />
 <strong>V1.0</strong>
-<br />- Première version
+<br />- Première (vrai) version
+<br />- Réécriture du code complet (mais même algo)
+<br />- Nouveau design
+<br />- Optimisation algo de génération
+<br />- Mobile proof
+
+<br />
+<strong>V1.0.1</strong>
+<br />- Mise a jours des compteurs de perms page membres
+
+<br />
+<strong>V1.0.2</strong>
+<br />- Correctifs mineurs (affichage)
+
+<br />
+<strong>V1.0.3</strong>
+<br />- Suppression des fichiers inutles
+<br />- Modifications sur l'algo de génération (bugs perms vide)
+
+<br />
+<strong>V1.1.1</strong>
+<br />- Planning du jour affiché par défaut (au lieu de tout le planning)
+<br />- Correction bug d'affichage du planning le dimanche
